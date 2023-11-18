6a5c700a258134b568a4143cf253f37327ed7f54
Ayesha2542 <123588676+Ayesha2542@users.noreply.github.com>
2023-11-18 15:24:57 -0800

    jhj
    jjhj

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const customReferences = require('./references/customReferences');
 require('./database/config');
 require('./controllers/Auth/CustomerAuthController');
-
+require('./controllers/Auth/adminAuthController');
 customReferences.app.listen(8888);
