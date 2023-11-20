f932091feaabb466d74a329e05d7c777ad59ad86
ToqeerFatima <125538720+ToqeerFatima@users.noreply.github.com>
2023-11-20 00:42:51 -0800

    qwert
    qwetrt

--- controllers/Auth/subAdminController.js
+++ controllers/Auth/subAdminController.js
@@ -39,3 +39,56 @@ customReferences.app.post("/addSubAdmin", formData.none(), async (request, respo
   }
 }
 );
+customReferences.app.post("/viewAllSubAdmins", async (request, response) => {
+  try {
+    const users = await subAdminModel.find(); // Retrieve all Customers from MongoDB
+    console.log("+++++");
+    console.log(users);
+    response.json(users);
+  } catch (error) {
+    response.status(500).json({ error: "Internal server error." });
+  }
+});
+
+
+customReferences.app.delete("/deleteSubAdmin/:subAdminId", async (request, response) => {
+  try {
+    const { subAdminId } = request.params;
+
+    // Find and delete the customer by ID
+    const deleteSubAdmin = await subAdminModel.findByIdAndDelete(subAdminId);
+
+    if (deleteSubAdmin) {
+      response.json({ success: true, message: "subAdmin deleted successfully." });
+    } else {
+      response.json({ success: false, message: "subAdmin not found." });
+    }
+  } catch (error) {
+    response.status(500).json({ error: "Internal server error." });
+  }
+});
+
+customReferences.app.put("/togglesubAdminStatus/:subAdminId", async (request, response) => {
+  try {
+    const { subAdminId } = request.params;
+
+    // Find the user by ID
+    const subAdmin = await subAdminModel.findById(subAdminId);
+
+    if (!subAdmin) {
+      return response.json({ success: false, message: "User not found." });
+    }
+
+    // Toggle the user status between 0 and 1
+    subAdmin.status = subAdmin.status === 1 ? 0 : 1;
+
+    // Save the updated user
+    const updatedsubAdmin = await subAdmin.save();
+
+    response.json({ success: true, message: "User status updated successfully.", updatedsubAdmin });
+  } catch (error) {
+    response.status(500).json({ error: "Internal server error." });
+  }
+});
+
+
